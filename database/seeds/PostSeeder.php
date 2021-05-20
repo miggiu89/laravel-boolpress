467b90990b2b7ea7157267c4274584252573a7ef
Matteo <matteomigliori280489@gmail.com>
2021-05-20 17:23:41 +0200

    push

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++){
+            $new_post = new POst();
+
+            $new_post->title = $faker->sentence(rand(2,6));
+            $new_post->content = $faker->text(rand(100,200));
+
+            // slug generator
+            $new_post->slug = Str::slug($new_post->title, '-');
+
+            $new_post->save();
+        }
+    }
+}
